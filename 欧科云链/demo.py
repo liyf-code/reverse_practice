45b64a697745c12a3df22dc9f010c1532a7bf1fb
liyf <git@gitlab.ersoft.cn>
2022-07-13 18:07:01 +0800

    修改为最后更新于: 2023-03-28 更新readme文档

---
+++ 欧科云链/demo.py
@@ -0,0 +1,37 @@
+# _*_ coding: utf-8 _*_
+# @Date:  5:56 下午
+# @File: demo.py
+# @Author: liyf
+
+import requests
+import execjs
+
+
+def get_apikey():
+    with open('demo.js', 'r') as f:
+        js_str = f.readlines()
+    ctx = execjs.compile(''.join(js_str))
+    return ctx.call('getApiKey')
+
+
+def get_json_data():
+    url = 'aHR0cHM6Ly93d3cub2tsaW5rLmNvbS96aC1jbi9idGMvdHgtbGlzdD9saW1pdD0yMCZwYWdlTnVtPTE='
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+        'x-apiKey': get_apikey()
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+
+def parse():
+    result = get_json_data()
+    data_list = result['data']['hits']
+    for data in data_list:
+        print(
+            f'交易哈希: {data["hash"]}\n所在区块: {data["blockHeight"]}\n输入: {data["inputsCount"]}\n输出: {data["outputsCount"]}\n数量(BTC): {data["realTransferValue"]}')
+        print('***' * 30)
+
+
+if __name__ == '__main__':
+    parse()
