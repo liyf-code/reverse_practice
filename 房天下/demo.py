9b38f3a9083ac2a7342db03bfe1c38373c3278c3
liyf <git@gitlab.ersoft.cn>
2022-07-13 18:32:53 +0800

    修改为最后更新于: 2023-03-28 修改py文件中的url地址为明文

--- 房天下/demo.py
+++ 房天下/demo.py
@@ -22,7 +22,7 @@ def get_json_data(user, pwd):
         'Accept-Language': 'zh-CN,zh;q=0.9',
         'Connection': 'keep-alive',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Referer': 'aHR0cHM6Ly9wYXNzcG9ydC5mYW5nLmNvbS8=',  # 这个请求头不能少，否则报错
+        'Referer': 'https://passport.fang.com/',  # 这个请求头不能少，否则报错
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
     }
     data = {
