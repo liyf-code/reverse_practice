75a3c7e6ff030bb207a7c6d5be2680ba3cdcb1a6
liyf <git@gitlab.ersoft.cn>
2022-07-28 17:01:33 +0800

    add yuanrenxue

---
+++ yuanrenxue/topic57/demo.js
@@ -0,0 +1,17 @@
+var CryptoJS = require('crypto-js')
+
+
+function I(X, l) {
+    var C = X
+        , q = CryptoJS.enc.Utf8.parse(C)
+        , v = CryptoJS.TripleDES.decrypt(l, q, {
+            'mode': CryptoJS.mode.ECB,
+            'padding': CryptoJS.pad.Pkcs7
+        });
+    return v.toString(CryptoJS.enc.Utf8);
+}
+
+function get_data_list(result) {
+    var datas = JSON.parse(I(result.slice(0, 8), result.slice(8)))['data']
+    return datas
+}
