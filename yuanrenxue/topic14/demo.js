679aecd4e6b1dc891c9a7d3089e815c985fb7d95
liyf <git@gitlab.ersoft.cn>
2022-07-29 14:34:30 +0800

    修改为最后更新于: 2023-03-28 add 猿人学第14题：js_fuck核心代码加密

---
+++ yuanrenxue/topic14/demo.js
@@ -0,0 +1,22 @@
+var CryptoJS = require('crypto-js')
+
+var window = {}
+
+
+function aes_encrypt(word) {
+    var k = 'wdf2ff*TG@*(F4)*YH)g430HWR(*)' + 'wse';
+    var key = CryptoJS.enc.Utf8.parse(k);
+    var srcs = CryptoJS.enc.Utf8.parse(word);
+    var encrypted = CryptoJS.AES.encrypt(srcs, key, {
+        mode: CryptoJS.mode.ECB,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    return encrypted.toString();
+}
+
+function get_uc(page){
+    window.t = Date.parse(new Date()) / 1000;
+    word = window.t + '|' + page
+    uc = aes_encrypt(word)
+    return uc
+}
