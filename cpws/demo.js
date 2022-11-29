4e610613e7d85afaa42b3f990a2516de8b812608
liyf <git@gitlab.ersoft.cn>
2022-11-29 15:31:07 +0800

    修改为最后更新于: 2023-03-28 裁判文书响应数据解密

--- cpws/demo.js
+++ cpws/demo.js
@@ -1,9 +1,27 @@
 window = global;
 var CryptoJS = require('crypto-js')
 
+Date.prototype.Format = function (fmt) { // author: meizz
+    var o = {
+        "M+": this.getMonth() + 1, // 月份
+        "d+": this.getDate(), // 日
+        "h+": this.getHours(), // 小时
+        "m+": this.getMinutes(), // 分
+        "s+": this.getSeconds(), // 秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), // 季度
+        "S": this.getMilliseconds() // 毫秒
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
 var DES3 = {
     iv: function () {
-        return '20221129'
+        // 本地改写，返回时间格式。eg：20221129
+        return new Date().Format("yyyyMMdd")
     },
     encrypt: function (b, c, a) {
         if (c) {
