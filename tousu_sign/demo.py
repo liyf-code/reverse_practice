31e0b2a1dac770cf9df8e2904ce7006c907a503d
liyf <git@gitlab.ersoft.cn>
2023-03-14 15:31:07 +0800

    修改为最后更新于: 2023-03-28 华为黑猫投诉平台，sgnature破解，webpack

--- tousu_sign/demo.py
+++ tousu_sign/demo.py
@@ -35,12 +35,12 @@ def get_results(page):
         'rs': req_params['rs'],
         'signature': req_params['signature'],
         'couid': '6384912431',
-        'type': '1',
+        'sid': '26873',
         'page_size': '10',
         'page': str(page),
     }
 
-    response = requests.get('https://tousu.sina.com.cn/api/company/received_complaints', params=params, headers=headers)
+    response = requests.get('https://tousu.sina.com.cn/api/company/service_complaints', params=params, headers=headers)
     return response.json()
 
 
@@ -56,11 +56,11 @@ def parse():
             zf = data['main']['share_amount']
             pl = data['main']['comment_amount']
             summary = data['main']['summary']
-            author = data['author']['title']
+            author = data['author']['title'] if 'title' in data['author'].keys() else '未知'
             ts = data['main']['timestamp']
-            dt = ts2time(int(ts))
+            # dt = ts2time(int(ts))
             print(
-                f'标题: {title}\n投诉人: {author} 在 {dt} 于黑猫投诉平台发起\n投诉对象: {cotitle}\n投诉要求: {appeal}\n投诉内容: {summary}\n点赞数（{dz}）评论数（{pl}）转发数（{zf}）')
+                f'标题: {title}\n投诉人: {author} 在 {ts} 于黑猫投诉平台发起\n投诉对象: {cotitle}\n投诉要求: {appeal}\n投诉内容: {summary}\n点赞数（{dz}）评论数（{pl}）转发数（{zf}）')
             print('===' * 35)
 
 
