7958796c652ca1b42c686e730bf9707939581015
liyf <git@gitlab.ersoft.cn>
2023-07-18 11:37:30 +0800

    网络者-反反爬虫练习平台第四题，JSFuck加密-初体验

---
+++ yuanrenxue/wangluozhe4/demo.js
@@ -0,0 +1,25 @@
+window = global;
+
+var CryptoJS = require('crypto-js');
+let date = Date.parse(new Date()),
+    key_tmp = date * 1234,
+    iv_tmp = date * 4321,
+    key = CryptoJS.enc.Utf8.parse(key_tmp),
+    iv = CryptoJS.enc.Utf8.parse(iv_tmp);
+(function tmp(date, key, iv) {
+    function Encrypt(word) {
+        let srcs = CryptoJS.enc.Utf8.parse(word);
+        let encrypted = CryptoJS.AES.encrypt(srcs, key, {
+            iv: iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        });
+        return encrypted.ciphertext.toString().toUpperCase();
+    }
+
+    window.sign = Encrypt(date);
+})(date, key, iv);
+
+function get_signature(){
+    return window.sign
+}
