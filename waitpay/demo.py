5dcc28e855a74180b7d31a73dd3158c52e9d6003
liyf <git@gitlab.ersoft.cn>
2023-09-20 14:46:18 +0800

    美食优惠聚合，已废弃

--- waitpay/demo.py
+++ waitpay/demo.py
@@ -60,10 +60,6 @@ def get_encrypt_data(page):
         'pragma': 'no-cache',
         'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
         'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
     }
     params = {
