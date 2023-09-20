b5590a010421fe1b261e74a1aa398d4d79b50595
liyf <git@gitlab.ersoft.cn>
2023-09-20 14:08:24 +0800

    中国招投标公共服务平台

--- ctbpsp/demo.py
+++ ctbpsp/demo.py
@@ -21,15 +21,8 @@ def get_results(page):
         'Cache-Control': 'no-cache',
         'Connection': 'keep-alive',
         'Origin': 'http://ctbpsp.com',
-        'Pragma': 'no-cache',
         'Referer': 'http://ctbpsp.com/',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'cross-site',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-        'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
     }
 
     response = requests.get(
