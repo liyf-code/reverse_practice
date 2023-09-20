b6a1951a506a2a419b1b9cad317f41aa2169c655
liyf <git@gitlab.ersoft.cn>
2023-09-20 14:50:15 +0800

    喜马拉雅，实现登录过程

--- xmly_login/demo.py
+++ xmly_login/demo.py
@@ -12,8 +12,6 @@ def get_nonce():
     headers = {
         'Accept': '*/*',
         'Accept-Language': 'zh-CN,zh;q=0.9',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive',
         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
     }
