90128d802267c11b253b679bf3dfe415d1071641
liyf <git@gitlab.ersoft.cn>
2023-09-20 13:57:20 +0800

    阿里文学网小说正文，响应数据解密

--- aliwx/demo.py
+++ aliwx/demo.py
@@ -12,7 +12,6 @@ from loguru import logger
 headers = {
     'authority': 'www.aliwx.com.cn',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-language': 'zh-CN,zh;q=0.9',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
 }
 
